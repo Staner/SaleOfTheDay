f68ae745a9f537ccf2d8bce1931ced0f1be998a6
Sergei Popov <sergei.mutd@gmail.com>
2015-09-22 16:38:58 +0300

    test3

---
+++ app/src/main/java/foundationСlasses/City.java
@@ -0,0 +1,71 @@
+package foundationСlasses;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Sergei on 9/14/2015.
+ */
+public class City {
+
+    String _id;
+    String name;
+    Coordinates coordinates;
+    ArrayList<ShopCenter> shopCenters;
+
+    public City(String _id, String name, Coordinates coordinates) {
+        this._id = _id;
+        this.name = name;
+        this.coordinates = coordinates;
+
+        shopCenters = new ArrayList<ShopCenter>();
+    }
+
+    public City() {
+    }
+
+    // create city constructor
+    public City( String name, Coordinates coordinates) {
+
+        this.name = name;
+        this.coordinates = coordinates;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(Coordinates coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public ArrayList<ShopCenter> getShopCenters() {
+        return shopCenters;
+    }
+
+    public void setShopCenters(ArrayList<ShopCenter> shopCenters) {
+        this.shopCenters = shopCenters;
+    }
+
+    public void addShopCenter(ShopCenter shopCenter) {
+
+        shopCenters.add(shopCenter);
+
+
+    }
+}
---
+++ app/src/main/java/foundationСlasses/Floor.java
@@ -0,0 +1,81 @@
+package foundationСlasses;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Sergei on 9/18/2015.
+ */
+public class Floor  {
+
+    String name;
+    String _id;
+    String cityId;
+    String shopCenterId;
+    ArrayList<Shop> shops;
+
+    public Floor(String _id, String name, String cityId, String shopCenterId) {
+        this._id = _id;
+        this.name = name;
+        this.cityId = cityId;
+        this.shopCenterId = shopCenterId;
+        shops = new ArrayList<Shop>();
+    }
+
+    public Floor() {
+    }
+
+    public Floor( String name, String cityId, String shopCenterId) {
+
+        this.name = name;
+        this.cityId = cityId;
+        this.shopCenterId = shopCenterId;
+
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(String cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getShopCenterId() {
+        return shopCenterId;
+    }
+
+    public void setShopCenterId(String shopCenterId) {
+        this.shopCenterId = shopCenterId;
+    }
+
+    public ArrayList<Shop> getShops() {
+        return shops;
+    }
+
+    public void setShops(ArrayList<Shop> shops) {
+        this.shops = shops;
+    }
+
+    public void addShop(Shop shop) {
+
+    shops.add(shop);
+
+    }
+
+}
