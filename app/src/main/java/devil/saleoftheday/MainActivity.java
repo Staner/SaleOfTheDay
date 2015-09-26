38d0033adfe4fdd8d68ff0f7b304c0ca45e1f3ca
Sergei Popov <sergei.mutd@gmail.com>
2015-09-26 13:13:41 +0300

    onClick

--- app/src/main/java/devil/saleoftheday/MainActivity.java
+++ app/src/main/java/devil/saleoftheday/MainActivity.java
@@ -23,12 +23,15 @@ import android.widget.Toast;
 import foundationСlasses.City;
 import foundationСlasses.ColorTool;
 import foundationСlasses.Floor;
+import foundationСlasses.Shop;
 import foundationСlasses.ShopCenter;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
 
     Button city, center, floor;
+
+    int counterEnter = 0;
 
     City selectedCity = new City();
     ShopCenter selectedShopCenter = new ShopCenter();
@@ -43,9 +46,6 @@ public class MainActivity extends Activity {
     ViewGroup vg;
 
 
-
-
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,180 +53,26 @@ public class MainActivity extends Activity {
 
         vg = (ViewGroup) findViewById(R.id.mainLayout);
 
-
-
-
-
-
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+
 
         Log.d("shopCenetr:", WelcomeActivity.PARSE_DATA.getData().get(0).getShopCenters().size() + "");
         Log.d("shopCenetr:", WelcomeActivity.PARSE_DATA.getData().get(1).getShopCenters().size() + "");
         Log.d("parse size", WelcomeActivity.PARSE_DATA.getData().size() + "");
 
         city = (Button) findViewById(R.id.btncity);
+        city.setOnClickListener(this);
         center = (Button) findViewById(R.id.btncenter);
+        center.setOnClickListener(this);
         floor = (Button) findViewById(R.id.btnfloor);
+        floor.setOnClickListener(this);
 
         iv = (ImageView) findViewById(R.id.image);
         ivMask = (ImageView) findViewById(R.id.image_areas);
 
-
-        city.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                AlertDialog.Builder builderSingle = new AlertDialog.Builder(
-                        MainActivity.this);
-
-                builderSingle.setTitle("נא לבחור עיר:");
-                final ArrayAdapter<String> cityArrayAdapter = new ArrayAdapter<String>(
-                        MainActivity.this,
-                        android.R.layout.select_dialog_singlechoice);
-                for (City city : WelcomeActivity.PARSE_DATA.getData()) {
-                    cityArrayAdapter.add(city.getName());
-                }
-
-                builderSingle.setAdapter(cityArrayAdapter,
-                        new DialogInterface.OnClickListener() {
-
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-
-                                city.setText(WelcomeActivity.PARSE_DATA.getData().get(which).getName());
-
-                                Log.d("name", WelcomeActivity.PARSE_DATA.getData().get(which).getName());
-                                Log.d("shop center size ", WelcomeActivity.PARSE_DATA.getData().get(which).getShopCenters().size() + "");
-
-                                if (WelcomeActivity.PARSE_DATA.getData().get(which).getShopCenters().size() > 0) {
-
-                                    center.setEnabled(true);
-
-                                    selectedCity.setName(WelcomeActivity.PARSE_DATA.getData().get(which).getName());
-                                    selectedCity.set_id(WelcomeActivity.PARSE_DATA.getData().get(which).get_id());
-                                    selectedCity.setCoordinates(WelcomeActivity.PARSE_DATA.getData().get(which).getCoordinates());
-                                    selectedCity.setShopCenters(WelcomeActivity.PARSE_DATA.getData().get(which).getShopCenters());
-
-                                    Log.d("selected name:", selectedCity.getName());
-                                    Log.d("ccity center:", selectedCity.getShopCenters().size() + "");
-                                } else {
-                                    center.setEnabled(false);
-                                    floor.setEnabled(false);
-                                }
-
-                            }
-                        });
-                builderSingle.show();
-
-
-            }
-        });
-
-
-        center.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                AlertDialog.Builder builderSingle = new AlertDialog.Builder(
-                        MainActivity.this);
-
-                builderSingle.setTitle("נא לבחור מרכז קניות");
-                final ArrayAdapter<String> shopCenterArrayAdapter = new ArrayAdapter<String>(
-                        MainActivity.this,
-                        android.R.layout.select_dialog_singlechoice);
-                for (ShopCenter shopCenter : selectedCity.getShopCenters()) {
-                    shopCenterArrayAdapter.add(shopCenter.getName());
-                }
-
-                builderSingle.setAdapter(shopCenterArrayAdapter,
-                        new DialogInterface.OnClickListener() {
-
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-
-                                center.setText(selectedCity.getShopCenters().get(which).getName());
-
-                                if (selectedCity.getShopCenters().get(which).getFloors().size() < 1) {
-
-                                    floor.setEnabled(false);
-
-                                    fillSelectedShopCenter(which);
-
-                                    iv.setImageBitmap(selectedShopCenter.getCenterImage());
-
-                                } else {
-                                    floor.setEnabled(true);
-                                    fillSelectedShopCenter(which);
-                                    iv.setImageBitmap(selectedShopCenter.getCenterImage());
-
-                                }
-
-
-                            }
-
-                            private void fillSelectedShopCenter(int which) {
-                                selectedShopCenter.set_id(selectedCity.getShopCenters().get(which).get_id());
-                                selectedShopCenter.setName(selectedCity.getShopCenters().get(which).getName());
-                                selectedShopCenter.setCityId(selectedCity.getShopCenters().get(which).getCityId());
-                                selectedShopCenter.setCoordinates(selectedCity.getShopCenters().get(which).getCoordinates());
-                                selectedShopCenter.setCenterImage(selectedCity.getShopCenters().get(which).getCenterImage());
-                                selectedShopCenter.setFloors(selectedCity.getShopCenters().get(which).getFloors());
-
-
-                            }
-                        });
-                builderSingle.show();
-
-
-            }
-        });
-
-        floor.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                AlertDialog.Builder builderSingle = new AlertDialog.Builder(
-                        MainActivity.this);
-
-                builderSingle.setTitle("floors:");
-                final ArrayAdapter<String> floorsArrayAdapter = new ArrayAdapter<String>(
-                        MainActivity.this,
-                        android.R.layout.select_dialog_singlechoice);
-                for (Floor floor : selectedShopCenter.getFloors()) {
-                    floorsArrayAdapter.add(floor.getName());
-                }
-
-                builderSingle.setAdapter(floorsArrayAdapter,
-                        new DialogInterface.OnClickListener() {
-
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-
-
-                                selectedFloor.set_id(selectedShopCenter.getFloors().get(which).get_id());
-                                selectedFloor.setName(selectedShopCenter.getFloors().get(which).getName());
-                                selectedFloor.setCityId(selectedShopCenter.getFloors().get(which).getCityId());
-                                selectedFloor.setShopCenterId(selectedShopCenter.getFloors().get(which).getShopCenterId());
-                                selectedFloor.setFloorMapImage(selectedShopCenter.getFloors().get(which).getFloorMapImage());
-                                selectedFloor.setFloorMaskImage(selectedShopCenter.getFloors().get(which).getFloorMaskImage());
-                                selectedFloor.setShops(selectedShopCenter.getFloors().get(which).getShops());
-
-                                iv.setImageBitmap(selectedFloor.getFloorMapImage());
-                                ivMask.setImageBitmap(selectedFloor.getFloorMaskImage());
-                                floor.setText(selectedShopCenter.getFloors().get(which).getName());
-                                imageCrosserMethod();
-
-
-                            }
-                        });
-                builderSingle.show();
-
-
-            }
-        });
-
-
     }
+
 
     @Override
     protected void onResume() {
@@ -251,8 +97,12 @@ public class MainActivity extends Activity {
         @Override
         public void onLocationChanged(Location location) {
 
-            checkLocation(location);
-            vg.invalidate();
+if (counterEnter==0) {
+    counterEnter++;
+    checkLocation(location);
+    vg.invalidate();
+}
+
 
         }
 
@@ -351,12 +201,13 @@ public class MainActivity extends Activity {
         iv.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent ev) {
-                boolean handledHere = false;
 
                 final int action = ev.getAction();
+                boolean handledHere = false;
 
                 final int evX = (int) ev.getX();
                 final int evY = (int) ev.getY();
+
                 int nextImage = -1;
 
                 ImageView imageView = (ImageView) v.findViewById(R.id.image);
@@ -384,31 +235,26 @@ public class MainActivity extends Activity {
 
                     case MotionEvent.ACTION_UP:
 
+
                         int touchColor = getHotspotColor(R.id.image_areas, evX, evY);
-
-
                         ColorTool ct = new ColorTool();
                         int tolerance = 25;
 
-                          /*    selectedShop = new Shop(
-                                selectedFloor.getShops().get(action).get_id(),
-                                selectedFloor.getShops().get(action).getCityId(),
-                                selectedFloor.getShops().get(action).getShopCenterId(),
-                                selectedFloor.getShops().get(action).getFloorId(),
-                                selectedFloor.getShops().get(action).getName(),
-                                selectedFloor.getShops().get(action).getColorTouch());
 
-                        selectedFloor.setShops(selectedShopCenter.getFloors().get(action).getShops());
-                       // selectedShop.setColorTouch(selectedFloor.getShops().get(action).getColorTouch());
+                        for (Shop shop : selectedFloor.getShops()) {
 
-                           // selectedFloor.getShops().get(action).getColorTouch();   */
+                            Log.d("count shops in floor", "+1");
+/*
+                    Log.d("touchColor", touchColor + "");
+                    Log.d("colorShop", shop.getColorTouch() + "");
+                    Log.d("colorShop", shop.getName() + "");
+                    Log.d("tolerance", tolerance + "");*/
 
-
-                        if (ct.closeMatch(Color.parseColor(selectedFloor.getShops().get(action).getColorTouch()), touchColor, tolerance)) {
-                            //    if (ct.closeMatch(Color.parseColor(selectedFloor.getShops().get(action).getColorTouch()), touchColor, tolerance))
-                            toast(selectedFloor.getShops().get(action).getName());
-                        } else {
-                            toast("try another object");
+                            if (ct.closeMatch(Color.parseColor(shop.getColorTouch()), touchColor, tolerance)) {
+                                Log.d("colorShop", shop.getName() + "");
+                                toast(shop.getName());
+                                return true;
+                            }
                         }
                         if (currentResource == nextImage) {
                             nextImage = d.getLevel();
@@ -420,6 +266,7 @@ public class MainActivity extends Activity {
                         handledHere = false;
                 }
 
+
                 if (handledHere) {
 
                     if (nextImage > 0) {
@@ -428,11 +275,9 @@ public class MainActivity extends Activity {
                     }
                 }
                 return handledHere;
+
             }
         });
-
-
-        toast("אנא בחר איזור");
 
 
     }
@@ -460,6 +305,148 @@ public class MainActivity extends Activity {
     } // end toast
 
 
+    @Override
+    public void onClick(View v) {
 
+        AlertDialog.Builder builderSingle = new AlertDialog.Builder(
+                MainActivity.this);
+
+        switch (v.getId()) {
+            case R.id.btncity:
+
+                builderSingle.setTitle("נא לבחור עיר:");
+                final ArrayAdapter<String> cityArrayAdapter = new ArrayAdapter<String>(
+                        MainActivity.this,
+                        android.R.layout.select_dialog_singlechoice);
+                for (City city : WelcomeActivity.PARSE_DATA.getData()) {
+                    cityArrayAdapter.add(city.getName());
+                }
+
+                builderSingle.setAdapter(cityArrayAdapter,
+                        new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                city.setText(WelcomeActivity.PARSE_DATA.getData().get(which).getName());
+
+                                Log.d("name", WelcomeActivity.PARSE_DATA.getData().get(which).getName());
+                                Log.d("shop center size ", WelcomeActivity.PARSE_DATA.getData().get(which).getShopCenters().size() + "");
+
+                                if (WelcomeActivity.PARSE_DATA.getData().get(which).getShopCenters().size() > 0) {
+
+                                    center.setEnabled(true);
+
+                                    selectedCity.setName(WelcomeActivity.PARSE_DATA.getData().get(which).getName());
+                                    selectedCity.set_id(WelcomeActivity.PARSE_DATA.getData().get(which).get_id());
+                                    selectedCity.setCoordinates(WelcomeActivity.PARSE_DATA.getData().get(which).getCoordinates());
+                                    selectedCity.setShopCenters(WelcomeActivity.PARSE_DATA.getData().get(which).getShopCenters());
+
+                                    Log.d("selected name:", selectedCity.getName());
+                                    Log.d("ccity center:", selectedCity.getShopCenters().size() + "");
+                                } else {
+                                    center.setEnabled(false);
+                                    floor.setEnabled(false);
+                                }
+
+                            }
+                        });
+                builderSingle.show();
+
+
+                break;
+            case R.id.btncenter:
+
+                builderSingle.setTitle("נא לבחור מרכז קניות");
+                final ArrayAdapter<String> shopCenterArrayAdapter = new ArrayAdapter<String>(
+                        MainActivity.this,
+                        android.R.layout.select_dialog_singlechoice);
+                for (ShopCenter shopCenter : selectedCity.getShopCenters()) {
+                    shopCenterArrayAdapter.add(shopCenter.getName());
+                }
+
+                builderSingle.setAdapter(shopCenterArrayAdapter,
+                        new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                center.setText(selectedCity.getShopCenters().get(which).getName());
+
+                                if (selectedCity.getShopCenters().get(which).getFloors().size() < 1) {
+
+                                    floor.setEnabled(false);
+
+                                    fillSelectedShopCenter(which);
+
+                                    iv.setImageBitmap(selectedShopCenter.getCenterImage());
+
+                                } else {
+                                    floor.setEnabled(true);
+                                    fillSelectedShopCenter(which);
+                                    iv.setImageBitmap(selectedShopCenter.getCenterImage());
+
+                                }
+
+
+                            }
+
+                            private void fillSelectedShopCenter(int which) {
+                                selectedShopCenter.set_id(selectedCity.getShopCenters().get(which).get_id());
+                                selectedShopCenter.setName(selectedCity.getShopCenters().get(which).getName());
+                                selectedShopCenter.setCityId(selectedCity.getShopCenters().get(which).getCityId());
+                                selectedShopCenter.setCoordinates(selectedCity.getShopCenters().get(which).getCoordinates());
+                                selectedShopCenter.setCenterImage(selectedCity.getShopCenters().get(which).getCenterImage());
+                                selectedShopCenter.setFloors(selectedCity.getShopCenters().get(which).getFloors());
+
+
+                            }
+                        });
+                builderSingle.show();
+
+
+                break;
+            case R.id.btnfloor:
+
+                builderSingle.setTitle("floors:");
+                final ArrayAdapter<String> floorsArrayAdapter = new ArrayAdapter<String>(
+                        MainActivity.this,
+                        android.R.layout.select_dialog_singlechoice);
+                for (Floor floor : selectedShopCenter.getFloors()) {
+                    floorsArrayAdapter.add(floor.getName());
+                }
+
+                builderSingle.setAdapter(floorsArrayAdapter,
+                        new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+
+                                selectedFloor.set_id(selectedShopCenter.getFloors().get(which).get_id());
+                                selectedFloor.setName(selectedShopCenter.getFloors().get(which).getName());
+                                selectedFloor.setCityId(selectedShopCenter.getFloors().get(which).getCityId());
+                                selectedFloor.setShopCenterId(selectedShopCenter.getFloors().get(which).getShopCenterId());
+                                selectedFloor.setFloorMapImage(selectedShopCenter.getFloors().get(which).getFloorMapImage());
+                                selectedFloor.setFloorMaskImage(selectedShopCenter.getFloors().get(which).getFloorMaskImage());
+                                selectedFloor.setShops(selectedShopCenter.getFloors().get(which).getShops());
+
+                                iv.setImageBitmap(selectedFloor.getFloorMapImage());
+                                ivMask.setImageBitmap(selectedFloor.getFloorMaskImage());
+                                floor.setText(selectedShopCenter.getFloors().get(which).getName());
+                                imageCrosserMethod();
+
+
+                            }
+                        });
+                builderSingle.show();
+
+
+                break;
+        }
+
+
+
+    }
 }
 
